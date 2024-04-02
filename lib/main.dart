ddb1bfc8746a300a8dbf5387416e95de6e0eb1ea
WilliamCruuz <cruzwilliam14@gmail.com>
2024-04-02 00:17:20 -0300

    Alterações

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
+      title: 'O primeiro app',
     );
   }
 }
