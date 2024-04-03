b37544d78d40b64870d8aee8e1d8cc3d55b2ce41
WilliamCruuz <cruzwilliam14@gmail.com>
2024-04-03 15:54:52 -0300

    Comitando os  arquivos

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int cont = 0;
 
+// Funções de decremento e incremento
   void decrementar() {
     setState(() {
       cont--;
@@ -40,99 +41,106 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+// Criando alguns Gets
   bool get isFull => cont == 50;
   bool get isEmpty => cont == 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // Texto 'Contador'
-          Padding(
-            padding: const EdgeInsets.only(bottom: 50),
-            child: Text(
-              isEmpty
-                  ? 'Nenhum aluno'
-                  : isFull
-                      ? 'Limite de alunos atingido!'
-                      : 'Quantidade de alunos',
-              style: TextStyle(
-                fontSize: 30,
-                color: isFull ? Colors.red : Colors.black,
-                fontWeight: FontWeight.w500,
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/background.jpg'),
+                fit: BoxFit.fill)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Texto 'Contador'
+            Padding(
+              padding: const EdgeInsets.only(bottom: 50),
+              child: Text(
+                isEmpty
+                    ? 'Nenhum aluno'
+                    : isFull
+                        ? 'Limite de alunos atingido!'
+                        : 'Quantidade de alunos',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: isFull ? Colors.red : Colors.white,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
             ),
-          ),
 
-          // Número do contador
-          Padding(
-            padding: const EdgeInsets.only(top: 50, bottom: 50),
-            child: Text(
-              cont.toString(),
-              style: TextStyle(
-                fontSize: 85,
-                color: isFull ? Colors.red : Colors.black,
-                fontWeight: FontWeight.w500,
+            // Número do contador
+            Padding(
+              padding: const EdgeInsets.only(top: 50, bottom: 50),
+              child: Text(
+                cont.toString(),
+                style: TextStyle(
+                  fontSize: 85,
+                  color: isFull ? Colors.red : Colors.white,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
             ),
-          ),
 
-          // Espaçamento
-          const SizedBox(height: 50),
+            // Espaçamento
+            const SizedBox(height: 50),
 
-          // Linha que contem os dois botões
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Primeiro botão na cor vermelha
-              TextButton(
-                onPressed: isEmpty ? null : decrementar,
-                style: TextButton.styleFrom(
-                  backgroundColor: isEmpty
-                      ? const Color.fromARGB(255, 245, 151, 145)
-                      : Colors.red,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(3),
+            // Linha que contem os dois botões
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Primeiro botão na cor vermelha
+                TextButton(
+                  onPressed: isEmpty ? null : decrementar,
+                  style: TextButton.styleFrom(
+                    backgroundColor: isEmpty
+                        ? const Color.fromARGB(255, 245, 151, 145)
+                        : Colors.red,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(3),
+                    ),
+                    minimumSize: const Size(100, 50),
                   ),
-                  minimumSize: const Size(100, 50),
-                ),
 
-                // Usando um icone no botão
-                child: const Icon(
-                  Icons.remove,
-                  size: 30,
-                ),
-              ),
-
-              // Espaçamento entre os botões
-              const SizedBox(
-                width: 80,
-              ),
-
-              // Segundo botão na cor verde
-              TextButton(
-                onPressed: isFull ? null : incrementar,
-                style: TextButton.styleFrom(
-                  backgroundColor: isFull
-                      ? const Color.fromARGB(255, 146, 216, 169)
-                      : Colors.green,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(3),
+                  // Usando um icone no botão
+                  child: const Icon(
+                    Icons.remove,
+                    size: 30,
                   ),
-                  minimumSize: const Size(100, 50),
                 ),
 
-                // Usando um icone no botão
-                child: const Icon(
-                  Icons.add,
-                  size: 30,
+                // Espaçamento entre os botões
+                const SizedBox(
+                  width: 80,
                 ),
-              ),
-            ],
-          )
-        ],
+
+                // Segundo botão na cor verde
+                TextButton(
+                  onPressed: isFull ? null : incrementar,
+                  style: TextButton.styleFrom(
+                    backgroundColor: isFull
+                        ? const Color.fromARGB(255, 146, 216, 169)
+                        : Colors.green,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(3),
+                    ),
+                    minimumSize: const Size(100, 50),
+                  ),
+
+                  // Usando um icone no botão
+                  child: const Icon(
+                    Icons.add,
+                    size: 30,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
